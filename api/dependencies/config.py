26362a2a1b1374bc7c013ab08f60277f12f5f38c
TeejMcSteez <tjhall047@gmail.com>
2025-06-24 13:24:06 -0400

    feat: added enviroment OS variables instead of hardcoding SQL password

--- api/dependencies/config.py
+++ api/dependencies/config.py
@@ -1,8 +1,16 @@
+import os
 class conf:
     db_host = "localhost"
     db_name = "sandwich_maker_api"
     db_port = 3306
     db_user = "root"
-    db_password = "rootroot"
+    """To set enviroment variables on . . .
+        BASH - export SQL_DB_PASSWORD="your_secret_password"
+        Windows (CMD) - set SQL_DB_PASSWORD=your_secret_password
+        Windows (Powershell) - $env:SQL_DB_PASSWORD = "your_secret_password"
+
+        This prevents hardcoding your SQL password.
+    """
+    db_password = os.getenv("SQL_DB_PASSWORD")
     app_host = "localhost"
     app_port = 8000
