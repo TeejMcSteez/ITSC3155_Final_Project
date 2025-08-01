0e6fc0a7fc4091d1e5e52e611ed529f4a71b1416
TeejMcSteez <tjhall047@gmail.com>
2025-08-01 11:09:21 -0400

    feat: added controller for payments

---
+++ api/controllers/payments.py
@@ -0,0 +1,79 @@
+from sqlalchemy.orm import Session
+from fastapi import HTTPException, status, Response, Depends
+from ..models import payments as model
+from sqlalchemy.exc import SQLAlchemyError
+# Used to auto-fill price from db
+from ..controllers import orders, order_details, sandwiches
+
+
+
+def create(db: Session, order_details_id, request):
+    od = order_details.read_one(db, order_details_id)
+    if od is None:
+        raise HTTPException(status_code=404, detail="Order not found!")
+    
+    sandwich = sandwiches.read_one(db, od.sandwich_id)
+    if sandwich is None:
+        raise HTTPException(status_code=404, detail="Sandwich not found!")
+    
+    new_item = model.Payments(
+        order_details_id=order_details_id,
+        order_id=od.order_id,
+        sandwich_id=od.sandwich_id,
+        sandwich_price=sandwich.first().price,
+        isPaid = False
+    )
+
+    try:
+        db.add(new_item)
+        db.commit()
+        db.refresh(new_item)
+    except SQLAlchemyError as e:
+        error = str(e.__dict__['orig'])
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST)
+    
+    return new_item
+
+def read_all(db: Session):
+    try:
+        result = db.query(model.Payments).all()
+    except SQLAlchemyError as e:
+        error = str(e.__dict__['orig'])
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST)
+    return result
+
+def read_one(db: Session, item_id):
+    try:
+        item = db.query(model.Payments).filter(model.Payments.id == item_id)
+        if not item:
+            raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST)
+    except SQLAlchemyError as e:
+        error = str(e.__dict__['orig'])
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST)
+    return item
+
+def update(db: Session, item_id, request):
+    try:
+        item = db.query(model.Payments).filter(model.Payments.id == item_id)
+        if not item.first():
+            raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST)
+        update_data = request.dict(exclude_unset=True)
+        item.update(update_data, synchronize_session=False)
+        db.commit()
+    except SQLAlchemyError as e:
+        error = str(e.__dict__['orig'])
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=error)
+    return item.first()
+# Added for sake of keeping spec, but deleting payment history isn't a good idea more than likely
+# Won't add a route for it but will be a controller in case their needs to be an auditing process
+def delete(db: Session, item_id):
+    try:
+        item = db.query(model.Payments).filter(model.Payments.id == item_id)
+        if not item.first():
+            raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST)
+        item.delete(synchronize_session=False)
+        db.commit()
+    except SQLAlchemyError as e:
+        error = str(e.__dict__['orig'])
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST)
+    return Response(status_code=status.HTTP_204_NO_CONTENT)
