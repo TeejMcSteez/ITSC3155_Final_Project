7009e5c235309d5239315d1388b929aa0497c8e3
TeejMcSteez <tjhall047@gmail.com>
2025-08-01 11:08:20 -0400

    feat: added payment schema

---
+++ api/schemas/payments.py
@@ -0,0 +1,25 @@
+from typing import Optional
+from pydantic import BaseModel
+from ..models.payments import Payments
+
+class PaymentsBase(BaseModel):
+    order_details_id: int
+    order_id: int
+    sandwich_id: int
+    isPaid: bool = False
+
+class PaymentsCreate(BaseModel):
+    pass
+
+class PaymentsUpdate(BaseModel):
+    isPaid: bool = True
+
+class PaymentsSchema(BaseModel):
+    id: int
+    order_details_id: int
+    order_id: int
+    sandwich_id: int
+    isPaid: bool
+
+    class ConfigDict:
+        from_attributes = True
