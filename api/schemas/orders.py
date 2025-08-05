14cbdab82619a558b81cb266cc6fd3b3352c879e
raremanticore <142352433+raremanticore@users.noreply.github.com>
2025-08-05 16:07:19 -0400

    Update to orders.py schema, added more parameters and OrderResponse method

--- api/schemas/orders.py
+++ api/schemas/orders.py
@@ -8,6 +8,9 @@ from .order_details import OrderDetail
 class OrderBase(BaseModel):
     customer_name: str
     description: Optional[str] = None
+    tracking_number: int
+    payment_status : bool
+    order_type : str
 
 
 class OrderCreate(OrderBase):
@@ -17,6 +20,8 @@ class OrderCreate(OrderBase):
 class OrderUpdate(BaseModel):
     customer_name: Optional[str] = None
     description: Optional[str] = None
+    payment_status: Optional[bool] = None
+    order_type: Optional[str] = None
 
 
 class Order(OrderBase):
@@ -26,3 +31,14 @@ class Order(OrderBase):
 
     class ConfigDict:
         from_attributes = True
+
+class OrderResponse(BaseModel):
+    id: int
+    customer_name: str
+    tracking_number: int
+    payment_status: bool
+    order_type: str
+    created_at: datetime
+
+    class Config:
+        orm_mode = True
