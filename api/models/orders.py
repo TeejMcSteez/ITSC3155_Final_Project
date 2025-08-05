875580bc3b48693215f6dcbdce8613b6a3c00236
raremanticore <142352433+raremanticore@users.noreply.github.com>
2025-08-05 16:17:37 -0400

    Updates to models/orders.py

--- api/models/orders.py
+++ api/models/orders.py
@@ -8,8 +8,10 @@ class Order(Base):
     __tablename__ = "orders"
 
     id = Column(Integer, primary_key=True, index=True, autoincrement=True)
-    customer_name = Column(String(100))
+    customer_name = Column(String(), nullable=False) #Changed this from String(100)
     order_date = Column(DATETIME, nullable=False, server_default=str(datetime.now()))
     description = Column(String(300))
+    tracking_number = Column(Integer, nullable=False)
+    payment_status = Column(Boolean, default=False)
 
     order_details = relationship("OrderDetail", back_populates="order")
